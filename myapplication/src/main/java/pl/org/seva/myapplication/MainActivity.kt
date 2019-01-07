524fb597c144acaa5c659bd0696c8b61d741c005
wiktor <wiktor.nizio@gmail.com>
2019-01-07 21:15:00 +0100

    New AS.

--- myapplication/src/main/java/pl/org/seva/myapplication/MainActivity.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/MainActivity.kt
@@ -8,6 +8,13 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        val onMapAvailable: Int.() -> Unit = {}
+
+        with (1) {
+            onMapAvailable()
+        }
+
         println("wiktor line 1")
         temp
         println("wiktor line 2")
