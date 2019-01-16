0e86471d6c07a6d73c73a20e0610ad79f7a88f20
wiktor <wiktor.nizio@gmail.com>
2019-01-16 09:11:40 +0100

    New AS and Kotlin.

--- myapplication/src/main/java/pl/org/seva/myapplication/MainActivity.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/MainActivity.kt
@@ -6,8 +6,14 @@ import android.widget.Toast
 
 class MainActivity: AppCompatActivity() {
 
+    private val a1 by instance<A>()
+    private val a2 by instance<A>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        println("wiktor $a1")
+        println("wiktor $a2")
 
         Toast.makeText(this, "wiktor", Toast.LENGTH_SHORT).show()
     }
