e4960d82eb978e8ad59850f10d24266fc6c94800
wiktor <wiktor.nizio@gmail.com>
2019-01-20 18:05:05 +0100

    Tested ProgressBar.

--- myapplication/src/main/java/pl/org/seva/myapplication/MainActivity.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/MainActivity.kt
@@ -1,20 +1,24 @@
 package pl.org.seva.myapplication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 
 class MainActivity: AppCompatActivity() {
 
-    private val a1 by instance<A>()
-    private val a2 by instance<A>()
-
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        fun onMessage(message: String) {
+            println("hello $message")
+        }
+
         super.onCreate(savedInstanceState)
+        val liveData = MutableLiveData<String>()
 
-        println("wiktor $a1")
-        println("wiktor $a2")
-
-        Toast.makeText(this, "wiktor", Toast.LENGTH_SHORT).show()
+        liveData.observe(this, Observer { onMessage(it) })
+        liveData.value = null
     }
 }
