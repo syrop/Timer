ff2e16b0d5ccf0f371de46b24a6e7d8efa60e6f8
wiktor <wiktor.nizio@gmail.com>
2019-01-28 16:52:32 +0100

    Tested nullability.

--- myapplication/src/main/java/pl/org/seva/myapplication/MainActivity.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/MainActivity.kt
@@ -3,22 +3,24 @@ package pl.org.seva.myapplication
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
+
+fun String.println() = println(this)
 
 class MainActivity: AppCompatActivity() {
 
+    var s: String? = null
+
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        fun onMessage(message: String) {
-            println("hello $message")
-        }
-
         super.onCreate(savedInstanceState)
-        val liveData = MutableLiveData<String>()
 
-        liveData.observe(this, Observer { onMessage(it) })
-        liveData.value = null
+        s = "wiktor foo"
+
+        if (s?.println() == null) {
+            println("wiktor null")
+        }
+        else {
+            println("wiktor not null")
+        }
     }
 }
