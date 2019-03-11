464691e0320b41ebe27c8f2768e04a4dc949bfda
wiktor <wiktor.nizio@gmail.com>
2019-03-11 12:59:52 +0100

    Fixed crashes.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainActivity.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainActivity.kt
@@ -3,19 +3,21 @@ package pl.org.seva.myapplication.main
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.util.*
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
+import pl.org.seva.myapplication.R
 
 class MainActivity: AppCompatActivity() {
 
+    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
 
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val a = mutableListOf(1)
-
-        println("wiktor ${!a.contains(1) && a.add(1)}")
-        println("wiktor ${!a.contains(2) && a.add(2)}")
-        println("wiktor ${Arrays.toString(a.toTypedArray())}")
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -1,27 +1,41 @@
 package pl.org.seva.myapplication.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_main.*
 import pl.org.seva.myapplication.R
 import pl.org.seva.myapplication.main.extension.inflate
 import pl.org.seva.myapplication.main.extension.observe
 import pl.org.seva.myapplication.main.extension.viewModel
+import java.util.concurrent.TimeUnit
 
 class MainFragment : Fragment() {
 
     private val model by viewModel<MainFragmentViewModel>()
-    
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
             inflate(R.layout.fragment_main, container)
 
+    @SuppressLint("SetTextI18n", "CheckResult")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        Observable.interval(20, TimeUnit.SECONDS, Schedulers.io())
+                .take(1)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    model.liveInt.value = 1
+                }
+
         model.liveInt.observe(this) { value ->
             println("wiktor int: $value")
+            text.text = "wiktor $value"
         }
     }
 }
